51f41706bc03acb61a73115db8d11afd3aa3d5f8
Jesus Urrutia <jesus.urrutia@gmail.com>
2012-03-13 11:58:40 -0300

    Primer Commit

---
+++ config/config-galeria.php
@@ -0,0 +1,9 @@
+<?php
+if(!defined('__GALERIA_COFIG__')):
+	define('__GALERIA_CONFIG__','');
+	
+	
+	define('TXT_TITULO_OWNER','Galeria');
+	define('TXT_TITULO_SITIO','');
+endif;
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require('config/config-galeria.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><?php echo TXT_TITULO_OWNER . ' - '. TXT_TITULO_SITIO; ?></title>
+<link type="text/css" href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<link type="text/css" href="libs/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />
+<script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+</body>
+</html>
